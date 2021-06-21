487cf7b3d5ffa2c388be5fb84de5848467492424
minako-y <m.tp2uk@gmail.com>
2021-06-21 13:51:22 +0900

    [Finish]2章基本文法

---
+++ 2章/app.js
@@ -0,0 +1,32 @@
+var hello = 'Hello World';
+alert(hello);
+
+// 整数
+var int1 = 1;
+// 負数
+var int2 = -10;
+// 小数点
+var float1 = 3.14;
+// 文字列
+var str1 = 'JavaScriptを覚えよう';
+
+alert(int1);
+
+alert(4+3);
+
+alert('Hello' + 'World');
+var str1 = 'Hello';
+var str2 = 'World!!';
+alert(str1 + str2);
+
+var orange = 100;
+var apple = 120;
+
+if(orange < apple){
+  alert('みかんの値段がりんごより安い');
+} else if(orange == apple){
+  alert('みかんとりんごが同じ値段');
+} else{
+  alert('みかんの値段がりんごより高い');
+}
+
---
+++ 2章/app4.js
@@ -0,0 +1,5 @@
+alert(4+3);
+alert(8-5);
+alert(2*6);
+alert(10/2);
+alert(5%3);
